ed16ea15ee9d2ed6a7c3ffac78c0fbf43dc974a7
dominic lizarraga <dominiclizarraga@hotmail.com>
2024-12-03 15:56:49 -0800

    Create internal component

--- src/components/RestaurantsGrid.jsx
+++ src/components/RestaurantsGrid.jsx
@@ -1,6 +1,20 @@
 export default function RestaurantsGrid({restaurants}) {
-  console.log(`hola`,restaurants)
+
   return (
-    <div>RestaurantsGrid</div>
+    <div>
+      <div>RestaurantsGrid</div>
+      {restaurants.map(restaurant => ( 
+        <RestaurantCard key={restaurant.id} restaurant={restaurant}/>
+        ))}
+    </div>
+
   )
+
+  function RestaurantCard({restaurant}) {
+    return (
+      <article>
+        <h4>{restaurant.name}</h4>
+      </article>
+    )
+  }
 }
